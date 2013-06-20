1f118a8268514579815b1b78e0c1248663a55932
Todd Wolfson <todd@twolfson.com>
2013-06-20 02:42:53 -0700

    Moving to request for assertion

--- test/listen-spawn_test.js
+++ test/listen-spawn_test.js
@@ -1,15 +1,15 @@
 // Load in module and dependencies
 var ListenSpawn = require('../lib/listen-spawn.js'),
     spawn = require('child_process').spawn,
-    assert = require('assert');
+    assert = require('assert'),
+    request = require('request');
 
-// TODO: Invoke child processes in separate processes to avoid polluting CLI
-// TODO: Or implement a proper `silent` option (depends on `events`)
 describe('ListenSpawn', function () {
   describe('starting an echo process', function () {
     before(function (done) {
       // Start up a new server
       var child = spawn('listen-spawn', ['date', '+%s']);
+      // var child = spawn('listen-spawn', ['date', '+%s'], {stdio: [0, 1, 2]});
 
       // Begin collecting stdout
       var that = this;
@@ -18,6 +18,9 @@ describe('ListenSpawn', function () {
         that.stdout += chunk;
       });
 
+      // Save the child for teardown
+      this.child = child;
+
       // Give us time to complete the startup
       setTimeout(done, 500);
     });
@@ -25,14 +28,29 @@ describe('ListenSpawn', function () {
     it('executes immediately', function () {
       // Assert stdout is near the current time
       var now = +new Date(),
-          stdoutDate = this.stdout.match(/\d{5,}/),
+          stdoutDate = this.stdout.match(/\d{5,}/g),
           then = +stdoutDate[0] * 1000;
       assert(Math.abs(now - then) < 5000);
     });
 
     describe('when touched', function () {
-      it('is executed again', function () {
+      before(function (done) {
+        // DEV: Request is a bit of overkill
+        request('http://localhost:3000/', done);
+      });
 
+      it('is executed again', function () {
+        console.log('HERE:', this.stdout);
+      });
+    });
+
+    // When we are done testing
+    after(function (done) {
+      // Teardown the child
+      var child = this.child;
+      child.kill();
+      child.on('exit', function (code) {
+        done();
       });
     });
   });
