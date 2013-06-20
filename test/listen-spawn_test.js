567e0e65753bcd2802cdd94fe7f242b447d1fe08
Todd Wolfson <todd@twolfson.com>
2013-06-20 02:08:15 -0700

    Wrote out test but it is failing to collect stdout =(

--- test/listen-spawn_test.js
+++ test/listen-spawn_test.js
@@ -1,7 +1,37 @@
-var listen_spawn = require('../lib/listen-spawn.js');
+// Load in module and dependencies
+var ListenSpawn = require('../lib/listen-spawn.js'),
+    assert = require('assert');
 
-describe('One', function () {
-  it('equals one', function () {
+// TODO: Invoke child processes in separate processes to avoid polluting CLI
+// TODO: Or implement a proper `silent` option (depends on `events`)
+describe('ListenSpawn', function () {
+  describe('starting an echo process', function () {
+    before(function (done) {
+      // Start up a new server
+      var server = new ListenSpawn('date', ['+%s']);
 
+      // Begin collecting stdout
+      var that = this;
+      this.stdout = '';
+      process.stdout.on('data', function (chunk) {
+        that.stdout += chunk;
+      });
+
+      // Give us time to complete the startup
+      setTimeout(done, 500);
+    });
+
+    it('executes immediately', function () {
+      // Assert stdout is near the current time
+      var now = +new Date(),
+          then = +this.stdout * 1000;
+      assert(now - then < 1000);
+    });
+
+    describe('when touched', function () {
+      it('is executed again', function () {
+
+      });
+    });
   });
 });
